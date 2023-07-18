493c606f9c5a8a2c23121fb4a0b90e0445c5a6e8
thelabcorner <thedabcorner@outlook.com>
2023-07-18 01:02:55 -0600

    Auto Balancing Feature - Cart Isolate Calc.
    - adds button to automatically balance THC / CBD value

--- calculatorJS/calculateCartridgeIsolate.js
+++ calculatorJS/calculateCartridgeIsolate.js
@@ -1,25 +1,23 @@
+function calculateCartridgeIsolate() {
+    const cartCount = parseFloat(document.getElementById("cartCount").value);
+    const cartCapacity = parseFloat(document.getElementById("cartCapacity").value);
+    const cbdPercentage = parseFloat(document.getElementById("cbdPercentage").value);
+    const thcPercentage = parseFloat(document.getElementById("thcaPercentage").value);
 
+    // Validate cart count and capacity
+    if (isNaN(cartCount) || isNaN(cartCapacity) || cartCount <= 0 || cartCapacity <= 0) {
+        const result = document.getElementById("calcResult");
+        result.innerHTML = "<br><br><span style='color: red'><b>Error: Invalid cart count or capacity</b></span><br>";
+        return;
+    }
 
-  function calculateCartridgeIsolate() {
-    const cartCount = document.getElementById("cartCount").valueAsNumber;
-    const cartCapacity = document.getElementById("cartCapacity").valueAsNumber;
-    const cbdPercentage = document.getElementById("cbdPercentage").valueAsNumber;
-    const thcPercentage = document.getElementById("thcaPercentage").valueAsNumber;
-
-        // Validate cart count and capacity
-        if (cartCount <= 0 || cartCapacity <= 0 || isNaN(cartCount) || isNaN(cartCapacity)) {
-            const result = document.getElementById("calcResult");
-            result.innerHTML = "<br><br><span style='color: red'><b>Error: Invalid cart count or capacity</b></span><br>";
-            return;
-        }
-
-          // Validate CBD and THC percentages
-          const totalPercentage = cbdPercentage + thcPercentage;
-          if (totalPercentage !== 100 || isNaN(totalPercentage)) {
-              const result = document.getElementById("calcResult");
-              result.innerHTML = "<br><br><span style='color: red'><b>Error: CBD and THC percentages must add up to 100%</b></span><br>";
-              return;
-          }
+    // Validate CBD and THC percentages
+    const totalPercentage = cbdPercentage + thcPercentage;
+    if (isNaN(totalPercentage) || totalPercentage !== 100) {
+        const result = document.getElementById("calcResult");
+        result.innerHTML = "<br><br><span style='color: red'><b>Error: CBD and THC percentages must add up to 100%</b></span><br>";
+        return;
+    }
 
     // Calculate total weight
     const totalWeight = cartCount * cartCapacity;
@@ -42,4 +40,58 @@
         " grams<br><b>CBD Required:</b> " +
         cbdWeight.toFixed(2) +
         " grams";
- }
+}
+
+function autoBalancePercentages() {
+    const cbdInput = document.getElementById("cbdPercentage");
+    const thcInput = document.getElementById("thcaPercentage");
+
+    let cbdPercentage = parseFloat(cbdInput.value);
+    let thcPercentage = parseFloat(thcInput.value);
+
+    if (isNaN(cbdPercentage) || isNaN(thcPercentage)) {
+        cbdPercentage = isNaN(cbdPercentage) ? 0 : cbdPercentage;
+        thcPercentage = isNaN(thcPercentage) ? 0 : thcPercentage;
+    }
+
+    // Normalize THC and CBD percentages if they are above 100%
+    if (thcPercentage > 100) {
+        thcPercentage = Math.floor(thcPercentage * 10) / 10; // Normalize to the nearest tenths place
+    }
+
+    if (cbdPercentage > 100) {
+        cbdPercentage = Math.floor(cbdPercentage * 10) / 10; // Normalize to the nearest tenths place
+    }
+
+
+    const totalPercentage = cbdPercentage + thcPercentage;
+
+    if (totalPercentage !== 100) {
+        const choice = prompt("The CBD and THC percentages do not add up to 100%. Please choose an adjustment method:\n\n'1' Original adjustment\n'2' Proportional adjustment");
+
+        if (choice === "1") {
+            // Original adjustment
+            if (cbdPercentage < thcPercentage) {
+                cbdPercentage += 100 - totalPercentage;
+            } else {
+                thcPercentage += 100 - totalPercentage;
+            }
+        } else if (choice === "2") {
+            // Proportional adjustment
+            const adjustmentFactor = (100 - totalPercentage) / totalPercentage;
+            cbdPercentage *= 1 + adjustmentFactor;
+            thcPercentage *= 1 + adjustmentFactor;
+        } else {
+            // Cancel operation
+            return;
+        }
+
+        cbdInput.value = cbdPercentage.toFixed(3);
+        thcInput.value = thcPercentage.toFixed(3);
+        calculateCartridgeIsolate();
+    }
+}
+
+
+
+
