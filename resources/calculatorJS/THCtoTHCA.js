34044ba603cd34053a90ac0be1e98442407d23a7
slooshied <thedabcorner@outlook.com>
2024-05-06 04:30:02 -0500

    update dcx-yield.html, input-finder.html, THCtoTHCA.js to use new bootstrap

--- resources/calculatorJS/THCtoTHCA.js
+++ resources/calculatorJS/THCtoTHCA.js
@@ -1,7 +1,14 @@
 function calculateThcToThca() {
     const thcWeight = document.getElementById("thcWeight").valueAsNumber;
     const thcaWeight = thcWeight / (314.224580195 / 358.21440943);
-    const formattedOutput = thcaWeight.toLocaleString(undefined, {
+    const lossCalc = thcaWeight - thcWeight;
+
+    const formattedThcaOutput = thcaWeight.toLocaleString(undefined, {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
+
+    const formattedLossOutput = lossCalc.toLocaleString(undefined, {
         minimumFractionDigits: 2,
         maximumFractionDigits: 2
     });
@@ -9,6 +16,6 @@ function calculateThcToThca() {
     if (isNaN(thcWeight) || thcWeight < 0.0000000000000000000000001 || thcWeight > 9999999999.9999999999) {
         document.getElementById("thcToThcaResult").innerHTML = "<br><br><span style='color: red'><b>Error: Invalid input value(s)<br>Note: Please enter a positive value between 0.1 and 10 billion.</b></span><br>";
     } else {
-        document.getElementById("thcToThcaResult").innerHTML = "<br><br><b>THCa required: </b><br>" + formattedOutput + " grams";
+        document.getElementById("thcToThcaResult").innerHTML = "<br><b>THCa required: </b><br>" + formattedThcaOutput + " grams" + "<br><br><b>Estimated CO2 Loss: </b><br>" + formattedLossOutput + " grams";
     }
 }
